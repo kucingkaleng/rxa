9130746eee7dd302eee5a235b8137648db362485
Ade <kucingkalenk@gmail.com>
2019-12-21 09:15:39 +0700

    loadJson and empty

--- src/RXA.php
+++ src/RXA.php
@@ -5,4 +5,17 @@ class RXA {
   public function greet (String $sName) {
     return 'Hi Hello ' . $sName . '! How are you doing today?';
   }
+
+  public static function loadJson ($json_path = null) {
+    $file = file_get_contents($json_path);
+    $json = json_decode($file);
+    return $json;
+  }
+
+  public static function empty ($val) {
+    if (in_array($val,['',null])) {
+      return true;
+    }
+    return false;
+  }
 }
